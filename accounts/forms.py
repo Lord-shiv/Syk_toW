5f16ba7b63c7309ff640483eee872abde84b1e23
shivam <shivamchouhan728@gmail.com>
2021-08-02 19:11:37 +0530

    👷~ Custom user model

---
+++ accounts/forms.py
@@ -0,0 +1,127 @@
+from django import forms
+from django.contrib.auth.models import Group
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
+from .models import User
+from django.contrib import messages
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username').lower()
+        try:
+            User.objects.get(username__exact=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError("This username is already taken.")
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        try:
+            account = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError(f"Email {email} is already in use.")
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password2"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('__all__')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class RegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username').lower()
+        try:
+            User.objects.get(username__exact=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError("This username is already taken.")
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is taken")
+        return email
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password2"])
+        if commit:
+            user.save()
+        return user
+
+
+class LogInForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError(
+                    "Invalid email or password")
+        else:
+            raise forms.ValidationError("Invlid Input.")
---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+urlpatterns = [
+    # path('signup/', SignUpView.as_view(), name='signup'),
+    # path('signin/', signIn, name='sign->in'),
+    # path('signout/', signOut, name='sign<-out'),
+]
